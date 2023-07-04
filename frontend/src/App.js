bcb2485871643377f41d2c6b2491ca78915eddfc
Hieu <chihieu20112001@gmail.com>
2023-07-04 17:41:06 +0700

    alo

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Mern</a>
+      </header>
+      <main>
+        List Product
+      </main>
+    </div>
+  );
+}
+
+export default App;
